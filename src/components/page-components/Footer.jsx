216bdbe24b429753ba614925de9e92304030f667
P HEMANTH KUMAR REDDY <93938266+hemanthreddy739@users.noreply.github.com>
2026-02-23 18:21:11 +0530

    updated footer

--- src/components/page-components/Footer.jsx
+++ src/components/page-components/Footer.jsx
@@ -4,9 +4,10 @@ import "../../css/Footer.css";
 function Footer() {
   return (
     <footer className="AppFooter">
-      <p> CloudMatrix's Note@pp &copy;2026 </p>
+      <p> CloudWorld's Note@pp &copy;2026 </p>
     </footer>
   );
 }
 
 export default Footer;
+    
